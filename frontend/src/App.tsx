5d5921f4cd6e5ad5ec0468e8cd71307a1b40ea05
João Neto <joao72neto@gmail.com>
2025-10-19 16:00:31 -0300

    test: criar teste cypress como exemplo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -79,7 +79,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button id="oh" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
