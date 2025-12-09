4c2f17026b8d384b03e6537e64d2947d46f0fa9a
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-12-09 13:57:32 -0300

    feat: ajustes nas chamadas de api do front

--- frontend/src/api/freelancerApi.ts
+++ frontend/src/api/freelancerApi.ts
@@ -1,5 +1,7 @@
 import type { Freelancer } from "../models/Freelancer";
 
+import { adicionarUsuario } from "./usuarioApi";
+
 const PATH = `${import.meta.env.VITE_API_URL}/api/Freelancer`;
 
 export const consultarFreelancers = async () => {
@@ -12,4 +14,34 @@ export const consultarFreelancers = async () => {
     const freelancers : Freelancer[] = await resposta.json();
 
     return freelancers;
+}
+
+export const consultarFreelancerPorId = async (id: number): Promise<Freelancer> => {
+    const resposta = await fetch(`${PATH}/${id}`);
+
+    if (resposta.status !== 200){
+        throw new Error("Erro ao consultar freelancer.");
+    }
+
+    return await resposta.json();
+}
+
+export const adicionarFreelancer = async (freelancer: Freelancer) => {
+    const usuarioAdicionado = await adicionarUsuario(freelancer);
+
+    freelancer.id = usuarioAdicionado.id;
+    
+    const option = {
+        method: 'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(freelancer)
+    }
+
+    const resposta = await fetch(PATH, option);
+
+    if (resposta.status !== 201) {
+        throw new Error("Erro ao adicionar freelancer.");
+    }
+
+    return resposta.json();
 }
