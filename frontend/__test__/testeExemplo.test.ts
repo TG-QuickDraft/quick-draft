a98edfaf2bec4ae4db7f42aaa5b0b81ecc964c34
João Neto <joao72neto@gmail.com>
2025-12-11 17:54:11 -0300

    chore: install and run prettier formatter

--- frontend/__test__/testeExemplo.test.ts
+++ frontend/__test__/testeExemplo.test.ts
@@ -1,11 +1,7 @@
-describe('Formatador', () => {
-
-  test('formata corretamente a data', () => {
-
-    expect(soma(2,3)).toEqual(5);
-    
+describe("Formatador", () => {
+  test("formata corretamente a data", () => {
+    expect(soma(2, 3)).toEqual(5);
   });
-
 });
 
 function soma(a: number, b: number): number {
--- frontend/src/api/usuarioApi.ts
+++ frontend/src/api/usuarioApi.ts
@@ -3,20 +3,20 @@ import type { Usuario } from "../models/Usuario";
 const PATH = `${import.meta.env.VITE_API_URL}/api/Usuario`;
 
 export const adicionarUsuario = async (usuario: Usuario) => {
-    const option = {
-        method: 'POST',
-        headers:{'Content-Type': 'application/json'},
-        body: JSON.stringify(usuario)
-    }
+  const option = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(usuario),
+  };
 
-    const resposta = await fetch(PATH, option);
+  const resposta = await fetch(PATH, option);
 
-    if (resposta.status !== 201) {
-        throw new Error("Erro ao adicionar usuário.");
-    }
+  if (resposta.status !== 201) {
+    throw new Error("Erro ao adicionar usuário.");
+  }
 
-    return resposta.json();
-}
+  return resposta.json();
+};
 
 export const enviarFoto = async (formData: FormData) => {
   return await fetch(`${PATH}/upload-foto`, {
