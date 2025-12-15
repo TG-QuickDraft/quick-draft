5239f7bb955b8e0eeb9ffac8ad60cc214f6cc77d
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-12-15 14:04:58 -0300

    style: mostrando descrição do serviço

--- frontend/src/pages/PesquisaServicos.tsx
+++ frontend/src/pages/PesquisaServicos.tsx
@@ -37,6 +37,7 @@ export function PesquisaServico() {
             <tr className="bg-white text-black">
               <th className="p-3">Id</th>
               <th className="p-3">Nome</th>
+              <th className="p-3">Descrição</th>
               <th className="p-3">Ir para Serviço</th>
             </tr>
           </thead>
@@ -48,6 +49,7 @@ export function PesquisaServico() {
               >
                 <td className="p-3">{servico.id}</td>
                 <td className="p-3">{servico.nome}</td>
+                <td className="p-3">{servico.descricao}</td>
                 <td className="p-3">
                   <Link to={`/visualizarServico/${servico.id}`}>
                     <Button>Ver Serviço</Button>
