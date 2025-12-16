afe255837d8cb3a687da5fdb7930930a00596f2e
João Neto <joao72neto@gmail.com>
2025-12-16 18:33:58 -0300

    fix(home): prevent content overflow

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -1,5 +1,5 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="p-8 h-screen text-white">{children}</div>;
+  return <div className="grid p-8 min-h-screen text-white">{children}</div>;
 };
 
 export default Container;
