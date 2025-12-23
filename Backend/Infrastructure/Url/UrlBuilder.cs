81075a9775413dd38901f48615bc052b981aa148
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-12-23 12:29:46 -0300

    feat: criação do UrlBuilder

---
+++ Backend/Infrastructure/Url/UrlBuilder.cs
@@ -0,0 +1,18 @@
+using Backend.Application.Interfaces.Infrastructure;
+using Backend.Infrastructure.Settings;
+using Microsoft.Extensions.Options;
+
+namespace Backend.Infrastructure.Url
+{
+    public class UrlBuilder(
+        IOptions<ImageSettings> options
+    ) : IUrlBuilder
+    {
+        private readonly ImageSettings _settings = options.Value;
+
+        public string ConstruirUrl(string path)
+        {
+            return $"{_settings.BaseUrl}/{path}";
+        }
+    }
+}
