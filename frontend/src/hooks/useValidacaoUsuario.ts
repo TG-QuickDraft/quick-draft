ac59802079cfbc27d25d36c5e6c699d649bd365c
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-12-23 17:32:18 -0300

    feat: criação do criarUsuarioSchema do Yup

--- frontend/src/hooks/useValidacaoUsuario.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-export const useValidacaoUsuario = () => {
-    return Yup.object().shape({
-        nome: Yup.string().required('Por favor, digite o seu nome!'),
-    })
-}
