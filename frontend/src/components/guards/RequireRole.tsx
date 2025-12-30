0c12f5aabb812fd7d75c7590b2208955dcfd2227
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-12-30 17:38:39 -0300

    feat(front): adição de componente RequireRole

---
+++ frontend/src/components/guards/RequireRole.tsx
@@ -0,0 +1,27 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
+import type { JSX } from "react";
+import { getRolesFromToken } from "@/utils/getRolesFromToken";
+
+type Props = {
+  roles: string[];
+  children: JSX.Element;
+};
+
+export const RequireRole = ({ roles, children }: Props) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  const userRoles = getRolesFromToken();
+
+  const hasRole = roles.some(r => userRoles.includes(r));
+
+  if (!hasRole) {
+    return <Navigate to="/403" replace />;
+  }
+
+  return children;
+};
