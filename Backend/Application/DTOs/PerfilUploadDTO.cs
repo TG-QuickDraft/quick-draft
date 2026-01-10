1516c43466f6b8d0958bb443d9f8efb783ff6c85
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-01-09 22:54:48 -0300

    refactor: criação de DTO para upload de imagem

--- Backend/Application/DTOs/PerfilUploadDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Backend.Application.DTOs
-{
-    public class PerfilUploadDTO
-    {
-        public required int UsuarioId { get; set; }
-        public required IFormFile FotoPerfil { get; set; }
-    }
-}
