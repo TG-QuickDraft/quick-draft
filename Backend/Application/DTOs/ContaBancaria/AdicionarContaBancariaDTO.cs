184d3afa307641e8441ba4f832766e35861aecc2
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-02-11 21:23:18 -0300

    feat: criação de dtos de conta bancária

---
+++ Backend/Application/DTOs/ContaBancaria/AdicionarContaBancariaDTO.cs
@@ -0,0 +1,12 @@
+namespace Backend.Application.DTOs.ContaBancaria
+{
+   public class CriarContaBancariaDTO
+    {
+        public required string CpfTitular { get; set; }
+        public required string NomeTitular { get; set; }
+        public required string Banco { get; set; }
+        public required string Agencia { get; set; }
+        public required string NumeroConta { get; set; }
+        public required string TipoConta { get; set; }
+    }
+}
