abf5bd6d6d869fcfeabebb4ba36e8492b6195853
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-02-14 11:11:07 -0300

    feat: chamadas de api

--- frontend/src/api/contaBancaria.api.ts
+++ frontend/src/api/contaBancaria.api.ts
@@ -1,7 +1,36 @@
+import type { ContaBancariaDTO } from "@/dtos/contaBancaria/ContaBancariaDTO";
 import type { CriarContaBancariaDTO } from "@/dtos/contaBancaria/CriarContaBancariaDTO";
 import { localStorageKeys } from "@/utils/localStorageKeys";
 
 const PATH = `${import.meta.env.VITE_API_URL}/api/contaBancaria`;
+
+export const consultarContaBancaria = async () => {
+    const option = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem(localStorageKeys.accessToken)}`,
+        },
+    }
+    
+    const resposta = await fetch(PATH, option);
+
+    if (resposta.status !== 200) {
+        throw new Error("Erro ao consultar conta.");
+    }
+
+    return await resposta.json();
+}
+
+export const consultarTiposConta = async () => {
+    const resposta = await fetch(`${PATH}/tipoConta`);
+
+    if (resposta.status !== 200) {
+        throw new Error("Erro ao consultar tipos de conta.");
+    }
+
+    return await resposta.json();
+}
 
 export const adicionarContaBancaria = async (conta: CriarContaBancariaDTO) => {
     const option = {
@@ -20,4 +49,23 @@ export const adicionarContaBancaria = async (conta: CriarContaBancariaDTO) => {
     }
 
     return resposta.json();
+}
+
+export const atualizarContaBancaria = async(dto: ContaBancariaDTO) => {
+  const option = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem(localStorageKeys.accessToken)}`,
+    },
+    body: JSON.stringify(dto),
+  };
+
+  const resposta = await fetch(PATH, option);
+  
+  if (resposta.status !== 200) {
+    throw new Error("Erro ao dto: ", (await resposta.json()));
+  }
+  
+  return resposta.json();
 }
