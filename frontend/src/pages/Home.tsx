e443f375fc3f9a8aa19495f77178a2f5c0424e2d
João Neto <joao72neto@gmail.com>
2026-02-18 17:31:59 -0300

    feat(home): add Banner and CreateAccountCard components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,14 @@
 import MockProfile from "@/assets/mock-profile.png";
+import clsx from "clsx";
 
 export const Home = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-80 bg-gray-200 shadow-inner"></div>
+      <Banner />
       <CreateAccountCard />
     </>
   );
 };
-
-import clsx from "clsx";
 
 export const CreateAccountCard = () => {
   const buttonClasses = clsx(
@@ -46,6 +45,35 @@ export const CreateAccountCard = () => {
 
         <p className={textClasses}>Criar conta</p>
       </button>
+    </div>
+  );
+};
+
+export const Banner = () => {
+  const containerClasses = clsx(
+    "flex flex-col items-center justify-center",
+    "h-80",
+    "bg-gradient-to-b",
+    "from-gray-700",
+    "via-gray-500",
+    "to-gray-400",
+    "shadow-inner",
+    "select-none",
+  );
+
+  const textClasses = clsx(
+    "text-4xl",
+    "text-center",
+    "max-w-[720px]",
+    "text-white",
+    "drop-shadow-[0_2px_6px_rgba(0,0,0,0.5)]",
+  );
+
+  return (
+    <div className={containerClasses}>
+      <p className={textClasses}>
+        Encontre os Melhores Freelancers Designers do Brasil Aqui!
+      </p>
     </div>
   );
 };
