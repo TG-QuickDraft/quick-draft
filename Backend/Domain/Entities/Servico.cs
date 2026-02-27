3f02c67ca958580361641169cde612ec38436cc9
fabi0casa <fabiocasa999@gmail.com>
2026-02-27 00:50:46 -0300

    feat: adicione as entidades restantes do serviço

--- Backend/Domain/Entities/Servico.cs
+++ Backend/Domain/Entities/Servico.cs
@@ -19,6 +19,25 @@ namespace Backend.Domain.Entities
         [Required]
         public string? Descricao { get; set; }
 
+        [Column("ser_orcamento", TypeName = "numeric(6,2)")]
+        [Required]
+        public decimal Orcamento { get; set; }
+
+        [Column("ser_prazo")]
+        [Required]
+        public DateTime Prazo { get; set; }
+
+        [Column("ser_valor_minimo", TypeName = "numeric(6,2)")]
+        [Required]
+        public decimal ValorMinimo { get; set; }
+
+        [Column("ser_is_entregue")]
+        [Required]
+        public bool IsEntregue { get; set; } = false;
+
+        [Column("ser_proposta_aceita_id")]
+        public int? PropostaAceitaId { get; set; }
+
         [Column("ser_cli_id")]
         [Required]
         public int ClienteId { get; set; }
