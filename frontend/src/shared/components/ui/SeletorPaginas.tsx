7298ebe843f328cd52b52c7f0107f3c945451900
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-01 16:16:32 -0300

    feat: criando componente SeletorPaginas e hook usePagination

---
+++ frontend/src/shared/components/ui/SeletorPaginas.tsx
@@ -0,0 +1,34 @@
+type Props = {
+    pagina: number;
+    totalPaginas: number;
+    onPaginaChange: (novaPagina: number) => void;
+}
+export const SeletorPaginas = ({
+  pagina,
+  totalPaginas,
+  onPaginaChange,
+}: Props) => {
+  return (
+    <div className="flex justify-end items-center gap-2 mt-4">
+      <button
+        className="px-3 py-1 border rounded disabled:opacity-50"
+        disabled={pagina === 1}
+        onClick={() => onPaginaChange(pagina - 1)}
+      >
+        Anterior
+      </button>
+
+      <span>
+        Página {pagina} de {totalPaginas}
+      </span>
+
+      <button
+        className="px-3 py-1 border rounded disabled:opacity-50"
+        disabled={pagina === totalPaginas}
+        onClick={() => onPaginaChange(pagina + 1)}
+      >
+        Próxima
+      </button>
+    </div>
+  );
+}
