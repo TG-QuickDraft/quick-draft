45c327e26738fb6c2dad2d49bc8f665765799192
fabi0casa <fabiocasa999@gmail.com>
2026-03-03 20:54:31 -0300

    feat: faça uma tela para a atualização dos dados do usuario

--- frontend/src/features/users/routes/usuario.routes.tsx
+++ frontend/src/features/users/routes/usuario.routes.tsx
@@ -2,10 +2,12 @@ import { CadastrarUsuario } from "@/features/users/pages/CadastrarUsuario";
 import LoginUsuario from "@/features/auth/pages/LoginUsuario";
 import { MinhaConta } from "@/features/auth/pages/MinhaConta";
 import { AtualizarSenha } from "@/features/auth/pages/AtualizarSenha";
+import { AtualizarDadosUsuario } from "@/features/auth/pages/AtualizarDadosUsuario";
 
 export const usuarioRoutes = [
   { path: "/login", element: <LoginUsuario /> },
   { path: "/cadastrarUsuario", element: <CadastrarUsuario /> },
   { path: "/minhaConta", element: <MinhaConta/>},
-  { path: "/atualizarSenha", element: <AtualizarSenha/>}
+  { path: "/atualizar-senha", element: <AtualizarSenha/>},
+  { path: "/atualizar-dados", element: <AtualizarDadosUsuario/>}
 ];
