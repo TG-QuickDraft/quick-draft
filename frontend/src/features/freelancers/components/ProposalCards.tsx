e59456f44be80423d9015ded92202ae4a952a51a
João Neto <joao72neto@gmail.com>
2026-03-04 18:51:28 -0300

    fix(proposal-card): prevent image overflow with overflow-hidden

--- frontend/src/features/freelancers/components/ProposalCards.tsx
+++ frontend/src/features/freelancers/components/ProposalCards.tsx
@@ -5,11 +5,11 @@ const ProposalCards = ({ img }: { img?: string }) => {
     <div
       className={clsx(
         "aspect-square w-100 bg-gray-400 rounded-xl border-2 border-gray-600/20",
-        "transition hover:scale-101 hover:shadow-xl cursor-pointer",
+        "transition hover:scale-101 hover:shadow-xl cursor-pointer overflow-hidden",
         "active:scale-98",
       )}
     >
-      <img src={img} className="w-full h-full object-cover rounded-xl" />
+      <img src={img} className="w-full h-full object-cover" />
     </div>
   );
 };
