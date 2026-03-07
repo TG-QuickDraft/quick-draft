34cb8099c360e7f0619f86b77dda5fcbd2d58d4e
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-07 16:46:16 -0300

    feat: implementando consulta e criação de propostas no backend

---
+++ Backend/Application/Services/PropostaService.cs
@@ -0,0 +1,89 @@
+using AutoMapper;
+using Backend.Application.DTOs.Proposta;
+using Backend.Application.Interfaces.Infrastructure;
+using Backend.Application.Interfaces.Repositories;
+using Backend.Application.Interfaces.Services;
+using Backend.Domain.Entities;
+
+namespace Backend.Application.Services
+{
+    public class PropostaService(
+        IPropostaRepository repository,
+        
+        IMapper mapper,
+        IUrlBuilder urlBuilder
+    ) : IPropostaService
+    {
+        private readonly IPropostaRepository _repository = repository;
+        private readonly IMapper _mapper = mapper;
+        private readonly IUrlBuilder _urlBuilder = urlBuilder;
+
+        public async Task<PropostaDTO?> ConsultarPorIdAsync(int id)
+        {
+            var proposta = _mapper.Map<PropostaDTO>(
+                await _repository.ConsultarPorIdAsync(id)
+            );
+
+            if (proposta == null)
+                return null;
+
+            return _mapper.Map<PropostaDTO>(proposta);
+        }
+
+        public async Task<IEnumerable<PropostaDTO>> ConsultarPorIdFreelancerAsync(int freelancerId)
+        {
+            IEnumerable<PropostaDTO> list = _mapper.Map<IEnumerable<PropostaDTO>>(
+                await _repository.ConsultarPorIdFreelancerAsync(freelancerId)
+            );
+
+            foreach (var proposta in list)
+            {
+                MapearUrls(proposta);
+            }
+
+            return list;
+        }
+
+        public async Task<IEnumerable<PropostaDTO>> ConsultarPorIdServicoAsync(
+            int servicoId
+        )
+        {
+            IEnumerable<PropostaDTO> list = _mapper.Map<IEnumerable<PropostaDTO>>(
+                await _repository.ConsultarPorIdServicoAsync(servicoId)
+            );
+
+            foreach (var proposta in list)
+            {
+                MapearUrls(proposta);
+            }
+
+            return list;
+        }
+
+        public async Task<PropostaDTO> CriarAsync(
+            CriarPropostaDTO dto,
+            int freelancerId
+        )
+        {
+            Proposta propostaToAdd = _mapper.Map<Proposta>(dto);
+            propostaToAdd.FreelancerId = freelancerId;
+
+            var propostaAdicionada = _mapper.Map<PropostaDTO>(
+                await _repository.CriarAsync(propostaToAdd)
+            );
+
+            MapearUrls(propostaAdicionada);
+
+            return _mapper.Map<PropostaDTO>(propostaAdicionada);
+        }
+
+        private void MapearUrls(PropostaDTO proposta)
+        {
+            foreach (var projeto in proposta.ProjetosDestacados)
+            {
+                projeto.ImagemUrl = _urlBuilder.ConstruirUrl(projeto.ImagemUrl ?? "");
+            }
+        }
+
+    }
+}
