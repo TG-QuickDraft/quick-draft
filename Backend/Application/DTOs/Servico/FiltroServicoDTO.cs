50347048156c33338a00695f3ff05962781639b7
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-15 11:58:08 -0300

    fix: pequena correção no FiltroServicoDTO

--- Backend/Application/DTOs/Servico/FiltroServicoDTO.cs
+++ Backend/Application/DTOs/Servico/FiltroServicoDTO.cs
@@ -3,9 +3,9 @@ namespace Backend.Application.DTOs.Servico
     public class FiltroServicoDTO
     {
         public string? Nome { get; set; }
-        public bool? OrcamentoIsAberto { get; set; } = true;
+        public bool? OrcamentoIsAberto { get; set; }
         public DateTime? PrazoMaximo { get; set; }
         public decimal? ValorMinimo { get; set; }
-        public bool? IsEntregue { get; set; } = false;
+        public bool? IsEntregue { get; set; }
     }
 }
