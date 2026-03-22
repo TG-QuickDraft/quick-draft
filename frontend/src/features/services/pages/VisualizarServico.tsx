cae00ba3f6c079f83c7b89063fd10ac941ff9585
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-21 22:29:06 -0300

    feat: melhorando a página de visualização de serviço

--- frontend/src/features/services/pages/VisualizarServico.tsx
+++ frontend/src/features/services/pages/VisualizarServico.tsx
@@ -8,69 +8,96 @@ import Title from "@/shared/components/ui/titles/Title";
 import Button from "@/shared/components/ui/buttons/Button";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import type { Servico } from "../dtos/Servico";
-import { IoMdSend } from "react-icons/io";
 import { freelancerPaths } from "@/features/freelancers/routes/freelancerPaths";
 import { clientePaths } from "@/features/clients/routes/clientePaths";
+import ProfilePhoto from "@/shared/components/ui/ProfilePhoto";
 import { servicoPaths } from "../routes/servicoPaths";
+import type { Cliente } from "@/features/clients/dtos/Cliente";
+import { consultarClientePorId } from "@/features/clients/api/cliente.api";
 
 export const VisualizarServico = () => {
   const { id } = useParams();
 
   const [servico, setServico] = useState<Servico | null>(null);
+  const [cliente, setCliente] = useState<Cliente | null>(null);
 
   useEffect(() => {
     const obterDados = async () => {
       const data = await consultarServicoPorId(Number(id));
+      const clienteData = await consultarClientePorId(data?.clienteId);
 
-      if (data !== undefined) {
+      if (data && clienteData) {
         setServico(data);
+        setCliente(clienteData);
       }
     };
 
     obterDados();
   }, [id]);
 
-  useEffect(() => {
-    console.log(servico);
-  }, [servico]);
-
   return (
-    <div className="h-full flex flex-1 items-center justify-center">
-      <div className="flex flex-col items-center gap-6">
-        <Title>Página de Serviço</Title>
-        <h3>{servico?.nome}</h3>
-        <h4>{servico?.descricao}</h4>
-        <h3>
-          {servico?.orcamentoIsAberto
-            ? "Orcamento aberto"
-            : "Orcamento fechado"}
-        </h3>
-        <h4>{servico?.valorMinimo}</h4>
-        <h3>{servico?.prazo}</h3>
+    <div className="w-full">
+      <Link to={servicoPaths.pesquisaServico}>
+        <Button
+          className="mt-6 bg-white border-0"
+          icon={<MdKeyboardDoubleArrowLeft className="text-black" size={30} />}
+        >
+          <Title className="text-black">Detalhes do Serviço</Title>
+        </Button>
+      </Link>
 
-        {servico?.clienteId && (
-          <Link to={clientePaths.perfilClienteById(servico.clienteId)}>
-            <Button className="mt-6">Ver Perfil do Cliente</Button>
-          </Link>
-        )}
+      <main className="flex gap-6 p-4 rounded-xl bg-neutral-10">
+        <section className="flex-1">
+          <div className="flex flex-col p-4 gap-1">
+            <Title>{servico?.nome}</Title>
+            <p>Valor mínimo: R$ {servico?.valorMinimo}</p>
+            <p>
+              {servico?.orcamentoIsAberto
+                ? "Orcamento aberto"
+                : "Orcamento fechado"}
+            </p>
+          </div>
 
-        {servico && (
-          <Link to={freelancerPaths.cadastrarPropostaById(servico.id)}>
-            <Button className="mt-6" icon={<IoMdSend size={25} />}>
-              Enviar proposta
-            </Button>
-          </Link>
-        )}
+          <div className="p-4 mt-4 rounded-2xl bg-white/90">
+            <Title className="mt-4">Descrição</Title>
+            <p className="my-6">{servico?.descricao}</p>
+          </div>
+        </section>
 
-        <Link to={servicoPaths.pesquisaServico}>
-          <Button
-            className="mt-6"
-            icon={<MdKeyboardDoubleArrowLeft size={30} />}
+        <section className="flex-1">
+          <Link
+            to={clientePaths.perfilClienteById(servico?.clienteId ?? "")}
+            className="flex justify-between items-center w-full"
           >
-            Voltar
-          </Button>
-        </Link>
-      </div>
+            <Title>Cliente: {cliente?.nome}</Title>
+            <ProfilePhoto
+              photoPath={cliente?.fotoPerfilUrl}
+              className="w-fit!"
+              imgClassName="h-30! w-30!"
+            />
+          </Link>
+
+          <hr className="m-4 text-neutral-40" />
+
+          <div>
+            <div className="flex items-center gap-2 my-2">
+              <p>Categoria</p>
+              <span className="flex-1 border-t-2 border-dashed" />
+              <p>{"AAAAA"}</p>
+            </div>
+
+            <div className="flex items-center gap-2 my-2">
+              <p>Prazo</p>
+              <span className="flex-1 border-t-2 border-dashed" />
+              <p>{servico?.prazo}</p>
+            </div>
+          </div>
+
+          <Link to={freelancerPaths.cadastrarPropostaById(servico?.id ?? "")}>
+            <Button>Enviar Proposta</Button>
+          </Link>
+        </section>
+      </main>
     </div>
   );
 };
