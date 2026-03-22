c4b023d153871d55a6c96821a400fa64ef397e6c
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-22 20:53:45 -0300

    feat: melhorias na página de VisualizarServico

--- frontend/src/features/services/pages/VisualizarServico.tsx
+++ frontend/src/features/services/pages/VisualizarServico.tsx
@@ -14,12 +14,17 @@ import ProfilePhoto from "@/shared/components/ui/ProfilePhoto";
 import { servicoPaths } from "../routes/servicoPaths";
 import type { Cliente } from "@/features/clients/dtos/Cliente";
 import { consultarClientePorId } from "@/features/clients/api/cliente.api";
+import { toLocaleString } from "@/shared/utils/date.utils";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import { numberToCurrency } from "@/shared/utils/number.utils";
 
 export const VisualizarServico = () => {
   const { id } = useParams();
 
   const [servico, setServico] = useState<Servico | null>(null);
   const [cliente, setCliente] = useState<Cliente | null>(null);
+
+  const { isAuthenticated, roles } = useAuth();
 
   useEffect(() => {
     const obterDados = async () => {
@@ -42,38 +47,35 @@ export const VisualizarServico = () => {
           className="mt-6 bg-white border-0"
           icon={<MdKeyboardDoubleArrowLeft className="text-black" size={30} />}
         >
-          <Title className="text-black">Detalhes do Serviço</Title>
+          <Title className="text-black">Voltar</Title>
         </Button>
       </Link>
 
-      <main className="flex gap-6 p-4 rounded-xl bg-neutral-10">
-        <section className="flex-1">
+      <main className="flex mt-6 gap-6 p-4 rounded-xl bg-neutral-10">
+        <section className="flex-1 p-4">
           <div className="flex flex-col p-4 gap-1">
             <Title>{servico?.nome}</Title>
-            <p>Valor mínimo: R$ {servico?.valorMinimo}</p>
-            <p>
-              {servico?.orcamentoIsAberto
-                ? "Orcamento aberto"
-                : "Orcamento fechado"}
-            </p>
+            <Title className="font-normal!">
+              {numberToCurrency(servico?.valorMinimo ?? 0)}
+            </Title>
           </div>
 
           <div className="p-4 mt-4 rounded-2xl bg-white/90">
             <Title className="mt-4">Descrição</Title>
-            <p className="my-6">{servico?.descricao}</p>
+            <p className="break-all my-6">{servico?.descricao}</p>
           </div>
         </section>
 
-        <section className="flex-1">
+        <section className="flex-1 p-4 mx-16">
           <Link
             to={clientePaths.perfilClienteById(servico?.clienteId ?? "")}
             className="flex justify-between items-center w-full"
           >
-            <Title>Cliente: {cliente?.nome}</Title>
+            <Title>Serviço criado por {cliente?.nome}</Title>
             <ProfilePhoto
               photoPath={cliente?.fotoPerfilUrl}
               className="w-fit!"
-              imgClassName="h-30! w-30!"
+              imgClassName="h-24! w-24!"
             />
           </Link>
 
@@ -83,19 +85,34 @@ export const VisualizarServico = () => {
             <div className="flex items-center gap-2 my-2">
               <p>Categoria</p>
               <span className="flex-1 border-t-2 border-dashed" />
-              <p>{"AAAAA"}</p>
+              <p>{"CATEGORIA"}</p>
             </div>
 
             <div className="flex items-center gap-2 my-2">
               <p>Prazo</p>
               <span className="flex-1 border-t-2 border-dashed" />
-              <p>{servico?.prazo}</p>
+              <p>{toLocaleString(servico?.prazo ?? "")}</p>
+            </div>
+
+            <div className="flex items-center gap-2 my-2">
+              <p>Orçamento</p>
+              <span className="flex-1 border-t-2 border-dashed" />
+              {servico?.orcamentoIsAberto === true ? (
+                <p className="text-green-500">Aberto</p>
+              ) : (
+                <p className="text-red-500">Fechado</p>
+              )}
             </div>
           </div>
 
-          <Link to={freelancerPaths.cadastrarPropostaById(servico?.id ?? "")}>
-            <Button>Enviar Proposta</Button>
-          </Link>
+          {isAuthenticated && roles.includes("Freelancer") && (
+            <Link
+              className="flex"
+              to={freelancerPaths.cadastrarPropostaById(servico?.id ?? "")}
+            >
+              <Button className="flex-1 mt-6 p-4!">Enviar Proposta</Button>
+            </Link>
+          )}
         </section>
       </main>
     </div>
