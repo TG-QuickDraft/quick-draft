371905a4b4b496c4aa58d18bb583d3091ffabc37
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-03-25 22:35:50 -0300

    feat: adicionando rota para a página de Chat

--- frontend/src/features/services/routes/servico.routes.tsx
+++ frontend/src/features/services/routes/servico.routes.tsx
@@ -3,9 +3,18 @@ import { CadastrarServico } from "@/features/services/pages/CadastrarServico";
 import PesquisaServico from "@/features/services/pages/PesquisaServicos";
 import { VisualizarServico } from "@/features/services/pages/VisualizarServico";
 import { servicoPaths } from "@/features/services/routes/servicoPaths";
+import { ChatServico } from "../pages/ChatServico";
 
 export const servicoRoutes = [
   { path: servicoPaths.pesquisaServico, element: <PesquisaServico /> },
+  {
+    path: servicoPaths.chatServico,
+    element: (
+      <RequireRole roles={["Cliente", "Freelancer"]}>
+        <ChatServico />
+      </RequireRole>
+    ),
+  },
 
   {
     path: `${servicoPaths.visualizarServico}/:id`,
--- frontend/src/features/services/routes/servicoPaths.ts
+++ frontend/src/features/services/routes/servicoPaths.ts
@@ -2,6 +2,7 @@ export const servicoPaths = {
   pesquisaServico: "/pesquisa-servico",
   visualizarServico: "/visualizar-servico",
   cadastrarServico: "/cadastrar-servico",
+  chatServico: "chat-servico",
 
   visualizarServicoById: (id: string | number) => `/visualizar-servico/${id}`,
 };
