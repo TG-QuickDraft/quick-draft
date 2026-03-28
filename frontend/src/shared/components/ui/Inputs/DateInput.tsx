e106ab0e075dd1f35edff0891fd7d8b99994e461
João Neto <joao72neto@gmail.com>
2026-03-28 11:34:43 -0300

    style(date-input): add smooth opening to calendar

--- frontend/src/shared/components/ui/Inputs/DateInput.tsx
+++ frontend/src/shared/components/ui/Inputs/DateInput.tsx
@@ -20,7 +20,7 @@ const DateInput = ({
   const hasError = Boolean(error);
 
   return (
-    <div className="flex gap-0.5 flex-col w-full">
+    <div className="relative flex gap-0.5 flex-col w-full">
       <DatePicker
         selected={selectedDate}
         onChange={onChange}
@@ -28,7 +28,7 @@ const DateInput = ({
         dateFormat="dd/MM/yyyy"
         placeholderText={placeholder || "Insira a data"}
         className={clsx(
-          "w-full px-4 py-3 rounded-lg outline-none ",
+          "w-full px-4 py-3 rounded-lg outline-none transition-all duration-200",
           "text-neutral-80 placeholder:text-neutral-40 bg-white",
           hasError
             ? "border border-error-1 focus:border-error-1 animate-shake"
