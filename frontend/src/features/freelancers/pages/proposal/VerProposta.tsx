1101802e1174a2437e4c74f40b805c7636788af4
João Neto <joao72neto@gmail.com>
2026-04-11 14:38:22 -0300

    feat(chat): adicionar botão de chat em propostas aceitas

--- frontend/src/features/freelancers/pages/proposal/VerProposta.tsx
+++ frontend/src/features/freelancers/pages/proposal/VerProposta.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import StarRating from "@/shared/components/ui/StarRating";
 import ProfilePhoto from "@/shared/components/ui/ProfilePhoto";
@@ -22,10 +22,14 @@ import Modal from "@/shared/components/ui/Modal";
 import { toLocaleString } from "@/shared/utils/date.utils";
 import { numberToCurrency } from "@/shared/utils/number.utils";
 import { BackButton } from "@/shared/components/ui/buttons/BackButton";
+import Button from "@/shared/components/ui/buttons/Button";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { servicoPaths } from "@/features/services/routes/servicoPaths";
 
 const VerProposta = () => {
   const { id } = useParams();
   const propostaId = Number(id);
+  const navigate = useNavigate();
 
   const [proposta, setProposta] = useState<PropostaDTO | null>(null);
   const [freelancer, setFreelancer] = useState<FreelancerDTO | null>(null);
@@ -103,25 +107,30 @@ const VerProposta = () => {
 
       <p className="text-gray-500 text-sm">Serviço: {servico?.nome}</p>
 
-      <div className="flex gap-5 items-center flex-wrap">
-        <div className="h-50 w-50 rounded-full">
-          <ProfilePhoto
-            photoPath={freelancer?.fotoPerfilUrl}
-            size="lg"
-            className="w-full! h-full!"
-            imgClassName="!w-full !h-full object-cover"
-          />
+      <div className="flex justify-between items-center">
+        <div className="flex gap-5 items-center flex-wrap">
+          <div className="h-50 w-50 rounded-full">
+            <ProfilePhoto
+              photoPath={freelancer?.fotoPerfilUrl}
+              size="lg"
+              className="w-full! h-full!"
+              imgClassName="!w-full !h-full object-cover"
+            />
+          </div>
+          <div>
+            <h1 className="text-3xl font-bold">
+              {freelancer?.nome || <Spinner />}
+            </h1>
+            <p className="text-[20px]">Design Gráfico / Editor de Vídeo</p>
+          </div>
+          <StarRating rating={4.2} />
         </div>
-
-        <div>
-          <h1 className="text-3xl font-bold">
-            {freelancer?.nome || <Spinner />}
-          </h1>
-
-          <p className="text-[20px]">Design Gráfico / Editor de Vídeo</p>
-        </div>
-
-        <StarRating rating={4.2} />
+        <Button
+          onClick={() => navigate(servicoPaths.chatServico)}
+          icon={<IoChatboxEllipsesOutline size={20} />}
+        >
+          Iniciar chat
+        </Button>
       </div>
 
       <div className="flex gap-5 flex-wrap">
