8d7c7bace32a8cc8c777d4a6b0154e5c74883eb7
João Neto <joao72neto@gmail.com>
2026-04-18 18:20:17 -0300

    feat(pagamento): adicionar modal de avaliação

--- frontend/src/shared/contexts/modal.context.tsx
+++ frontend/src/shared/contexts/modal.context.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import type { ReactNode } from "react";
-import Modal from "../components/ui/Modal";
+import Modal from "../components/ui/modals/Modal";
 import { useNavigate } from "react-router-dom";
 import type { ModalVariants } from "../types/ModalVariants";
 
