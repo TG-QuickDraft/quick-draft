15d38c5771ba4d32572041c78e15284a739e8ddd
NattanSouza404 <nattanssouzapvz@gemail.com>
2026-05-02 13:42:55 -0300

    fix: removendo alvoId do DTO de criarAvaliacao

--- Backend/Application/DTOs/Avaliacao/CriarAvaliacaoDTO.cs
+++ Backend/Application/DTOs/Avaliacao/CriarAvaliacaoDTO.cs
@@ -3,7 +3,6 @@ namespace Backend.Application.DTOs.Avaliacao
     public class CriarAvaliacaoDTO
     {
         public int ServicoId { get; set; }
-        public int AlvoId { get; set; }
         public int NotaEstrelas { get; set; }
         public string? Comentario { get; set; }
     }
