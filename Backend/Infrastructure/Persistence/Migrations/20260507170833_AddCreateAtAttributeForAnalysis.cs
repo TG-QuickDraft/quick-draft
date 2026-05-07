6f5c58571bac5c84ec5784c8d32c408edf161531
João Neto <joao72neto@gmail.com>
2026-05-07 14:13:19 -0300

    fix: restore creation date migrations

---
+++ Backend/Infrastructure/Persistence/Migrations/20260507170833_AddCreateAtAttributeForAnalysis.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreateAtAttributeForAnalysis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "usu_create_at",
+                table: "usuarios",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ser_create_at",
+                table: "servicos",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "pjf_create_at",
+                table: "projetos_freelancer",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ent_create_at",
+                table: "entregas",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "usuarios",
+                keyColumn: "usu_id",
+                keyValue: -1,
+                column: "usu_create_at",
+                value: new DateTime(2026, 5, 7, 17, 8, 33, 27, DateTimeKind.Utc).AddTicks(1122));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "usu_create_at",
+                table: "usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "ser_create_at",
+                table: "servicos");
+
+            migrationBuilder.DropColumn(
+                name: "pjf_create_at",
+                table: "projetos_freelancer");
+
+            migrationBuilder.DropColumn(
+                name: "ent_create_at",
+                table: "entregas");
+        }
+    }
+}
